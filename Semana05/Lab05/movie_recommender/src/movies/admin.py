76e9b5707280b37b4ffa6baca97a3ac00a1744b5
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-16 16:35:07 -0500

    actualización de admin.py

--- Semana05/Lab05/movie_recommender/src/movies/admin.py
+++ Semana05/Lab05/movie_recommender/src/movies/admin.py
@@ -163,3 +163,20 @@ class RatingAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'movie__title')
     readonly_fields = ['created_at', 'updated_at']
     autocomplete_fields = ['user', 'movie']
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin configuration for user profiles"""
+    list_display = ('user', 'favorite_genre_count', 'rating_count')
+    search_fields = ('user__username',)
+    filter_horizontal = ('favorite_genres', 'favorite_movies')
+    actions = [create_recommendation_admin_action()]
+    
+    def favorite_genre_count(self, obj):
+        """Count favorite genres"""
+        return obj.favorite_genres.count()
+    favorite_genre_count.short_description = "Favorite Genres"
+    
+    def rating_count(self, obj):
+        """Count ratings"""
+        return obj.user.ratings.count()
+    rating_count.short_description = "Ratings"
