c0f3b415480d0ab40bbf6012996d481a6799eea5
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-16 16:01:39 -0500

    creación de la app y definido los modelos y relaciones

--- Semana05/Lab05/movie_recommender/src/config/settings.py
+++ Semana05/Lab05/movie_recommender/src/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 TEMPLATES = [
     {
