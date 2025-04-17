a0ee453bbb080bcea96df82648bfac0185c80cdc
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-17 15:59:10 -0500

    Creación y actualización del login y logout mas template base relacionado

--- Semana05/Lab05/movie_recommender/src/users/views.py
+++ Semana05/Lab05/movie_recommender/src/users/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from users.forms import CustomUserCreationForm
+from django.contrib.auth import login,logout
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            return redirect('movie_list')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('movie_list')
