7ad9850b8ff764453ffb1d5c8a70b849f4ab6261
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-17 02:45:30 -0500

    App review creada y modelo de reseña creado para la app

---
+++ Semana05/Lab05/movie_recommender/src/reviews/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    rating = models.IntegerField()  # Puedes hacer una validación para que sea de 1 a 5
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title} ({self.rating}/5)"
