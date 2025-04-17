cceeb12bbbd7016f432845f8d1381bb2c8593f25
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-17 02:47:13 -0500

    Vistas creadas para reviews

--- Semana05/Lab05/movie_recommender/src/reviews/views.py
+++ Semana05/Lab05/movie_recommender/src/reviews/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Review
+from movies.models import Movie
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Listar reseñas de una película
+def movie_reviews(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    reviews = movie.reviews.all()
+    return render(request, 'reviews/movie_reviews.html', {'movie': movie, 'reviews': reviews})
+
+# Crear nueva reseña
+@login_required
+def add_review(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        rating = int(request.POST['rating'])
+
+        Review.objects.create(
+            movie=movie,
+            user=request.user,
+            title=title,
+            content=content,
+            rating=rating
+        )
+        return redirect('movie_reviews', movie_id=movie.id)
+
+    return render(request, 'reviews/add_review.html', {'movie': movie})
