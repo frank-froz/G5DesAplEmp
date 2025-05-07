7eef9761f121be3ca14e8c8204c5f39832764f49
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-07 18:27:42 -0500

    Update config/settigns.py

--- Semana08/quiz_api/config/settings.py
+++ Semana08/quiz_api/config/settings.py
@@ -37,7 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'rest_framework',
+    
+    # Local apps
+    'quizzes',
 ]
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # For development - change in production
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
