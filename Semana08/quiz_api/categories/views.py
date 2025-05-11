835989d5e2f05d7770557783bdb8bf583e8c5047
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-05-11 05:50:52 -0500

    se agregó view.py en categories

--- Semana08/quiz_api/categories/views.py
+++ Semana08/quiz_api/categories/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Category, Tag
+from .serializers import CategorySerializer, TagSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """ViewSet for Category model"""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    """ViewSet for Tag model"""
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
