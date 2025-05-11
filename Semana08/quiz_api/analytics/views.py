fb9670bc096aa3ecf63b8b8f2c4f9bf4748ffb2a
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-10 22:49:21 -0500

    Creación de las vistas de analytics

--- Semana08/quiz_api/analytics/views.py
+++ Semana08/quiz_api/analytics/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import QuestionStat, QuizActivity
+from .serializers import QuestionStatSerializer, QuizActivitySerializer
 
-# Create your views here.
+class QuestionStatViewSet(viewsets.ModelViewSet):
+    """ViewSet para obtener estadísticas de las preguntas"""
+    queryset = QuestionStat.objects.all()
+    serializer_class = QuestionStatSerializer
+
+class QuizActivityViewSet(viewsets.ModelViewSet):
+    """ViewSet para obtener la actividad de los cuestionarios"""
+    queryset = QuizActivity.objects.all()
+    serializer_class = QuizActivitySerializer
