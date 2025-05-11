3ad4bcc136311a0efb85a6a48d8d144dbe2cc83b
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-10 22:50:49 -0500

    inclusión de la url de analytics a  config/urls.py

--- Semana08/quiz_api/config/urls.py
+++ Semana08/quiz_api/config/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/analytics/', include('analytics.urls')),
 ]
