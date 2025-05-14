dcc6f18f622bbaa9d25cca9f45e5ddf3827f7f40
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-14 16:07:31 -0500

    Arreglar contenido repetitivo en el filtro

--- Semana09/sin-e-spoiler/src/components/FeaturedMovie.jsx
+++ Semana09/sin-e-spoiler/src/components/FeaturedMovie.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const FeaturedMovie = ({ movie }) => {
   return (
     <div className="featured-movie">
-      <div className="featured-movie__backdrop">
-        <img src={movie.backdrop} alt={movie.title} />
-      </div>
       <div className="featured-movie__content">
-        <h2>{movie.title}</h2>
-        <p>{movie.description}</p>
         <button className="btn btn--large">Book Now</button>
       </div>
     </div>
