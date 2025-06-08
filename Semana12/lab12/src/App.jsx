d6d55f29aa51a9b3ba7d2d5d764f1a0ebc79799b
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-07 19:50:22 -0500

    Conecta App principal con nuevas páginas de series

--- Semana12/lab12/src/App.jsx
+++ Semana12/lab12/src/App.jsx
@@ -1,12 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomPage'
+import CategoryPage from './pages/CategoryPage'
+import SeriePage from './pages/SeriePage'
+import SerieFormPage from './pages/SerieFormPage'
+import CategoryFormPage from './pages/category/CategoryFormPage'
+import CategoryEditFormPage from './pages/category/CategoryEditFormPage'
 
 function App() {
 
   return (
-    <>
-      <h1>Laboratorio 12</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<LoginPage />} />
+        <Route path='/home' element ={< HomePage />} />
+        <Route path='/categories' element ={< CategoryPage />} />
+        <Route path='/categories/new' element ={< CategoryFormPage />} />
+        <Route path='/categories/edit/:id' element ={< CategoryEditFormPage />} />
+        <Route path='/series' element ={< SeriePage />} />
+        <Route path='/series/new' element ={< SerieFormPage />} />
+        <Route path='/series/edit/:idserie' element ={< SerieFormPage />} />
+      </Routes>
         
-    </>
+    </BrowserRouter>
   )
 }
 
