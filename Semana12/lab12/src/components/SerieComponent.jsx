186e33e6696b0611720be558168dfb5fd33a5a29
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-07 19:50:39 -0500

    Agrega componentes para listado y formulario de series

---
+++ Semana12/lab12/src/components/SerieComponent.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom"
+
+function SerieComponent(props) {
+  const navigate = useNavigate()
+  const gotoUrl = (codigo) => {
+    navigate("/series/edit/" + codigo)
+  }
+  return (
+    <div className="card">
+      <img 
+        className="card-img-top" 
+        src={props.imagen 
+          ? ("http://localhost:8000/" + props.imagen) 
+          : "https://dummyimage.com/400x2   50/000/fff&text=Sin+imagen"} 
+        alt={props.nombre} 
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.nombre}</h5>
+        <p className="card-text">{props.categoria}</p>
+        <div className="d-flex justify-content-between">
+          <button onClick={() => gotoUrl(props.codigo)} className="btn btn-secondary">Editar</button> 
+          <button className="btn btn-danger">Eliminar</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SerieComponent
