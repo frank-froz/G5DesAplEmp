3c7c7132e1fe46ed29c04bbc2dc19bdf052bd2c1
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-18 16:00:54 -0500

    Footer, Header, Bostrap

---
+++ Semana14/sin-e-spoiler/src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <main>
+      
+    </main>
+      <h2>Las mejores peliculas de la historia</h2>
+    <Footer />
+    </>
+  )
+}
+
+export default App
